f36ffaf1f72faa1a5bdd88c3610443561f41131a
zhenni-17 <ST10468354@vcconnect.edu.za>
2025-06-19 09:03:21 +0200

    put arrays
    put some arrays

--- app/src/main/java/za/co/varsitycollege/st10468354/musicapp2025/DetailedViewScreen.kt
+++ app/src/main/java/za/co/varsitycollege/st10468354/musicapp2025/DetailedViewScreen.kt
@@ -1,16 +1,38 @@
 package za.co.varsitycollege.st10468354.musicapp2025
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 
 class DetailedViewScreen : AppCompatActivity() {
+    private val titles = arrayOf("Midnight", "Die With A Smile", "Birds Of A Feather", "Perfect")
+    private val artists = arrayOf("Taylor Swift", "Bruno Mars", "Billie Eilish", "Ed Sheeran")
+    private val ratings = intArrayOf(3, 5, 4, 5)
+    private val comments = arrayOf("Love", "Memories", "Calm", "Deep Love")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detailed_view_screen)
 
+        val btnDisplay = findViewById<Button>(R.id.btnDisplay)
+        val btnRating = findViewById<Button>(R.id.btnRating)
+        val btnReturn = findViewById<Button>(R.id.btnReturn)
+
+        btnDisplay.setOnClickListener {
+
+        }
+
+        btnRating.setOnClickListener {
+
+        }
+
+        btnReturn.setOnClickListener {
+
+        }
     }
 }
--- app/src/main/java/za/co/varsitycollege/st10468354/musicapp2025/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10468354/musicapp2025/MainActivity.kt
@@ -1,22 +1,51 @@
 package za.co.varsitycollege.st10468354.musicapp2025
 
+import android.app.ProgressDialog.show
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val titles = arrayOf("Midnight", "Die With A Smile", "Birds Of A Feather", "Perfect")
+    private val artists = arrayOf("Taylor Swift", "Bruno Mars", "Billie Eilish", "Ed Sheeran")
+    private val ratings = intArrayOf(3, 5, 4, 5)
+    private val comments = arrayOf("Love", "Memories", "Calm", "Deep Love")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        
+
+        // created variables
         val btnAdd = findViewById<Button>(R.id.btnAdd)
         val btnGo = findViewById<Button>(R.id.btnGo)
         val btnExit = findViewById<Button>(R.id.btnExit)
 
+        btnAdd.setOnClickListener {
+            addSongs()
+        }
+
+        btnGo.setOnClickListener {
+            val intent = Intent(this, DetailedViewScreen::class.java)
+            startActivity(intent)
+            intent.putExtra("songTitles", titles)
+            intent.putExtra("artistNames", artists)
+            intent.putExtra("songRatings", ratings)
+            intent.putExtra("commentSongs", comments)
+        }
+
+        btnExit.setOnClickListener {
+            finish()
+        }
+    }
+
+    // functions to add songs
+    private fun addSongs() {
 
     }
 }
