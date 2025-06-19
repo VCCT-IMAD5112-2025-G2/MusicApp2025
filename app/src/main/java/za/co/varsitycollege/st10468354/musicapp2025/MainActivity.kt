39fe96f6bc97d842d2134bd969ecfef1612bd6c4
zhenni-17 <ST10468354@vcconnect.edu.za>
2025-06-19 08:13:40 +0200

    Initial commit

---
+++ app/src/main/java/za/co/varsitycollege/st10468354/musicapp2025/MainActivity.kt
@@ -0,0 +1,16 @@
+package za.co.varsitycollege.st10468354.musicapp2025
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
