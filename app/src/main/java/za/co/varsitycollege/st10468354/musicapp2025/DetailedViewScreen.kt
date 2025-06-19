f68872dd9e506cf3be87dba213c0a33b7e6d5ddb
zhenni-17 <ST10468354@vcconnect.edu.za>
2025-06-19 08:31:54 +0200

    activity main
    Created layout

--- app/src/main/java/za/co/varsitycollege/st10468354/musicapp2025/DetailedViewScreen.kt
+++ app/src/main/java/za/co/varsitycollege/st10468354/musicapp2025/DetailedViewScreen.kt
@@ -1,22 +1,16 @@
 package za.co.varsitycollege.st10468354.musicapp2025
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class DetailedViewScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        
-        val btnAdd = findViewById<Button>(R.id.btnAdd)
-        val btnGo = findViewById<Button>(R.id.btnGo)
-        val btnExit = findViewById<Button>(R.id.btnExit)
-
+        setContentView(R.layout.activity_detailed_view_screen)
 
     }
 }
